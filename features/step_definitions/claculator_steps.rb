fa0218b88dc45cb2c362c1343d2972ae87d7819c
arisu <tsungying721@gmail.com>
2015-06-14 14:41:59 +0800

    Edit the step definitions
    ＊ 這邊要特別注意 @output = `ruby calc.rb #{@input}`
    ＊ ruby calc.rb #{@input} 是 Shell command，告訴 Ruby 你想要執行這行指令
    ＊ 左右加上的是``而不是單引號''

--- features/step_definitions/claculator_steps.rb
+++ features/step_definitions/claculator_steps.rb
@@ -1,9 +1,14 @@
-Given(/^the input "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^the input "([^"]*)"$/) do |input|
+  # 將 feature 所輸入的值儲存在 ＠input
+  # instance variable 可以繼續被使用在之後的 step 中
+  @input = input
 end
 
 When(/^the claculator is run$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  # 嘗試去執行 calc.rb，傳遞儲存在第一步的值，並將第二步輸出結果紀錄起來
+  @output = `ruby calc.rb #{@input}`
+  # 檢查 Ruby 變數 $? 來確認指令是否成功
+  raise("Command faild!") unless $?.success?
 end
 
 Then(/^the output should be "([^"]*)"$/) do |arg1|
